83426c1288052376a3e64a497428cdaa3942d518
root <root@DESKTOP-V8E1TNL.localdomain>
2020-08-19 00:16:26 -0300

    make todolist model require name and status

--- odoo/addons/todolist/models/models.py
+++ odoo/addons/todolist/models/models.py
@@ -8,9 +8,9 @@ class todolist(models.Model):
     _rec_name = 'title'
     _order = 'title'
 
-    title = fields.Char(size=50)
+    title = fields.Char(size=50, required=True)
     description = fields.Text()
-    status = fields.Selection([('01_created', 'Created'), ('02_working', 'Working'), ('03_done', 'Done'), ('99_problem', 'Problem')])
+    status = fields.Selection([('01_created', 'Created'), ('02_working', 'Working'), ('03_done', 'Done'), ('99_problem', 'Problem')], required="True", default="01_created")
     partner = fields.Many2one(comodel_name='res.partner')
     due = fields.Datetime()
     color = fields.Integer(compute='_color')
